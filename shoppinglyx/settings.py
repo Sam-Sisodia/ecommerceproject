052c2dbc7fb4e26797aa6ef98d2e5c35ee1b18bf
sam <sam@example.com>
2023-03-21 22:19:10 +0530

    add products and db

--- shoppinglyx/settings.py
+++ shoppinglyx/settings.py
@@ -75,27 +75,26 @@ WSGI_APPLICATION = 'shoppinglyx.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-
-'''DATABASES = {
+DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
-}'''
-
-
-
-DATABASES = {
-   'default': {
-    'ENGINE': 'django.db.backends.mysql',
-    'NAME': 'Myshop',
-    'USER' :'root',
-    'PASSWORD':"",
-     'HOST': '127.0.0.1',
-    'PORT': '3306',
-
-   }
 }
+
+
+
+# DATABASES = {
+#    'default': {
+#     'ENGINE': 'django.db.backends.mysql',
+#     'NAME': 'Myshop',
+#     'USER' :'root',
+#     'PASSWORD':"",
+#      'HOST': '127.0.0.1',
+#     'PORT': '3306',
+
+#    }
+# }
 
 
 
